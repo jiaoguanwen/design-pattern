3b6f3c35e0c3df47540ebce1c8e9f4c8a6b69aa4
jiaoguanwen <jiaoguanwen@163.com>
2018-08-01 22:07:56 +0800

    add module

---
+++ src/Person.js
@@ -0,0 +1,14 @@
+export default class Person {
+    constructor(name, age) {
+        this.name = name
+        this.age = age
+    }
+
+    eat() {
+        alert(`${this.name} eat something`)
+    }
+
+    speak() {
+        alert(`My name is ${this.name}, age ${this.age}`)
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
-class Person {
-    constructor(name) {
-        this.name = name
-    }
-    getName() {
-        return this.name
-    }
-}
+import Person from './Person'
+import Student from './Student'
 
-let p = new Person('jiaoguanwen')
-alert(p.getName())
+let zhang = new Person('zhang', 25)
+let wang = new Person('wang', 21)
+let li = new Student('li', 12, 'A1')
+
+zhang.eat()
+zhang.speak()
+
+wang.eat()
+wang.speak()
+
+li.eat()
+li.speak()
+li.study()
